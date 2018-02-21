eb2de86b311bf00f3e463532f98917efa4760945
Richard Conway <richard@rconway.co.uk>
2018-02-21 21:45:52 +0000

    Added some react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
+import { Route, Switch, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 import Richard from "./Richard";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        ...navbar...
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/users">Users</Link>
+        </nav>
+      </div>
+    );
+  }
+}
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
@@ -12,11 +39,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">zzzWelcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar />
         <hr/>
-        <Richard />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/users" component={Richard} />
+        </Switch>
       </div>
     );
   }
