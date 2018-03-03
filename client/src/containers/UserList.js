861094bdbc54c4f718a01dd26212fdbf19ec6424
Richard Conway <richard@rconway.co.uk>
2018-03-03 11:11:18 +0000

    First attempt at combining fetch with redux

--- client/src/containers/UserList.js
+++ client/src/containers/UserList.js
@@ -4,68 +4,77 @@ import { connect } from "react-redux";
 import { actionSetUsers } from "../actions/actions";
 import AddUserForm from "../components/AddUserForm";
 
+function fetchUsers() {
+  var p = new Promise(function(resolve, reject) {
+    fetch("/users").then(function(response) {
+      return response.json();
+    }).then(function(users) {
+      resolve(users);
+    });
+  });
+  return p;
+}
+
 class UserList extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.handleAddUser = this.handleAddUser.bind(this);
-    }
+    this.handleAddUser = this.handleAddUser.bind(this);
+  }
 
-    componentDidMount() {
-        if (!this.props.users || this.props.users.length === 0) {
-            var self = this;
-            /* global fetch */
-            fetch("/users").then(function(response) {
-                return response.json();
-            }).then(function(users) {
-                self.props.setUsers(users);
-            });
-        }
+  componentDidMount() {
+    if (!this.props.users || this.props.users.length === 0) {
+      var self = this;
+      /* global fetch */
+      fetchUsers().then(function(users) {
+        self.props.setUsers(users);
+      });
     }
-    
-    handleAddUser(user) {
-        var self = this;
-        var postData = user;
-        fetch("/users/add", {
-            method: "post",
-            headers: {
-              "content-type": "application/json"  
-            },
-            body: JSON.stringify(postData)
-        }).then(function(response) {
-            return response.json();
-        }).then(function(users) {
-            self.props.setUsers(users);
-        });
-    }
+  }
 
-    render() {
-        var userList;
-        if (this.props.users) {
-            // Sort by age descending
-            userList = this.props.users.slice().sort(function(a, b) {
-                return b.age - a.age;
-            })
-            // Output as list items.
-            userList = userList.map(function(user) {
-                return <li key={user.id}>{user.name} : {user.age}</li>;
-            });
-        }
-        return (
-            <div>
-                ...User List here...
-                <AddUserForm onSubmit={this.handleAddUser} />
-                <hr/>
-                { userList && userList.length>0 ? (
-                    <ul>
-                        {userList}
-                    </ul>
-                ) : (
-                    <div>Loading users...</div>
-                )}
-            </div>
-        );
+  handleAddUser(user) {
+    var self = this;
+    var postData = user;
+    fetch("/users/add", {
+      method: "post",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(postData)
+    }).then(function(response) {
+      return response.json();
+    }).then(function(users) {
+      self.props.setUsers(users);
+    });
+  }
+
+  render() {
+    var userList;
+    if (this.props.users) {
+      // Sort by age descending
+      userList = this.props.users.slice().sort(function(a, b) {
+        return b.age - a.age;
+      })
+      // Output as list items.
+      userList = userList.map(function(user) {
+        return <li key={user.id}>{user.name} : {user.age}</li>;
+      });
     }
+    return (
+      <div>
+        ...User List here...
+        <AddUserForm onSubmit={this.handleAddUser} />
+        <hr/>
+        { userList && userList.length>0 ? (
+          <ul>
+            {userList}
+          </ul>
+        ) : (
+          <div>Loading users...</div>
+        )}
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
